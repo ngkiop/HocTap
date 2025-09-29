d7c01c24257472ddac522aa1cb5be3828c8282d7
Ngk <ngocztoong18@gmail.com>
2025-09-29 16:25:04 +0700

    Bài 2: Máy tính 4 phép tính cơ bản

---
+++ calc.py
@@ -0,0 +1,11 @@
+a = float(input("Nhập số thứ nhất: "))
+b = float(input("Nhập số thứ hai: "))
+
+print("Cộng:", a + b)
+print("Trừ:", a - b)
+print("Nhân:", a * b)
+if b != 0:
+    print("Chia:", a / b)
+else: 
+    print("Chia: Không thể chia cho 0")
+

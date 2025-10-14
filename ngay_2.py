9324c3cf57f5f4dd6ef31b3f0828ae626fa527d5
Ngk <ngocztoong18@gmail.com>
2025-10-14 16:48:26 +0700

    Hoàn thành ngày 2

---
+++ ngay_2.py
@@ -0,0 +1,17 @@
+"""
+1. Phép toán cơ bản
+
+Nhập vào hai số nguyên a, b.
+
+In ra: a + b, a - b, a * b, a / b, a // b, a % b.
+"""
+
+a = int(input("Nhập số nguyên a: "))
+b = int(input("Nhập số nguyên b: "))
+
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a // b =", a // b)
+print("a % b =", a % b)

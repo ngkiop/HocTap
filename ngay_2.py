1e701e69119488edca11040c3e6bb8e150974d2e
Ngk <ngocztoong18@gmail.com>
2025-10-15 16:48:42 +0700

    Cập nhật ngày 2 thêm

--- ngay_2.py
+++ ngay_2.py
@@ -86,6 +86,7 @@ Nếu bằng nhau thì in "Hai số bằng nhau".
 
 # Trả lời cơ bản
 
+"""
 a = int(input("Nhập số nguyên a: "))
 b = int(input("Nhập số nguyên b: "))
 
@@ -95,5 +96,92 @@ elif a < b:
     print(f"{b} lớn hơn {a}")
 else:
     print("Hai số bằng nhau")
+"""
 
+# Nâng cấp 1
+
+"""
+a = int(input("Nhập số nguyên a: "))
+b = int(input("Nhập số nguyên b: "))
+c = int(input("Nhấp số nguyên c: "))
+
+lon_nhat = max(a, b, c)
+nho_nhat = min(a, b, c)
+
+print(f"Số lớn nhất là: {lon_nhat}")
+print(f"Số nhỏ nhất là: {nho_nhat}")
+"""
+
+"""
+a = int(input("Nhập số nguyên a: "))
+b = int(input("Nhập số nguyên b: "))
+c = int(input("Nhấp số nguyên c: "))
+
+lon_nhat = a
+nho_nhat = a
+
+if b > lon_nhat:
+    lon_nhat = b
+if c > lon_nhat:
+    lon_nhat = c
+
+if b < nho_nhat:
+    nho_nhat = b
+if c < nho_nhat:
+    nho_nhat = c
+
+print(f"Số lớn nhất là: {lon_nhat}")
+print(f"Số nhỏ nhất là: {nho_nhat}")
+"""
+
+# Nâng cấp 2
+
+"""
+a = int(input("Nhập số nguyên a: "))
+b = int(input("Nhập số nguyên b: "))
+c = int(input("Nhập số nguyên c: "))
+
+day_so = [a, b, c]
+day_so.sort()
+
+print("Số nhỏ nhất:", day_so[0])
+print("Số ở giữa:", day_so[1])
+print("Số lớn nhất:", day_so[2])
+"""
+
+"""
+a = int(input("Nhập số nguyên a: "))
+b = int(input("Nhập số nguyên b: "))
+c = int(input("Nhấp số nguyên c: "))
+
+lon_nhat = max(a, b, c)
+nho_nhat = min(a, b, c)
+
+if a != lon_nhat and a != nho_nhat:
+    so_o_giua = a
+elif b != lon_nhat and b != nho_nhat:
+    so_o_giua = b
+else:
+    so_o_giua = c
+
+print(f"Số lớn nhất là: {lon_nhat}")
+print(f"Số ở giữa là: {so_o_giua}")
+print(f"Số nhỏ nhất là: {nho_nhat}")
+"""
+
+"""
+day_so = list(map(int, input("Nhập dãy số, cách nhau bời dấu cách: ").split()))
+
+print(f"Số lớn nhất là: {max(day_so)}")
+print(f"Số nhỏ nhất là: {min(day_so)}")
+print(f"Giá trị trung bình là: {sum(day_so) / len(day_so):.2f}")
+"""
+
+# Nâng cấp 3
+
+
+day_so = list(map(int, input("Nhập dãy số, cách nhau bởi dấu cách: ").split()))
+
+print("Dãy tăng dần:", sorted(day_so))
+print("Dãy giảm dần:", sorted(day_so, reverse=True))
 

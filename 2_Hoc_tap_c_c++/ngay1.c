d5f4a3bbef5d3f43752559dd1d98e9b37377074d
Ngk <ngocztoong18@gmail.com>
2026-03-28 22:55:46 +0700

    Cap nhat them ngay 1

--- 2_Hoc_tap_c_c++/ngay1.c
+++ 2_Hoc_tap_c_c++/ngay1.c
@@ -1,10 +1,15 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main()
-// {
+int main()
+{
 
-//     printf("Hello World !.\n");
+    // printf("Hello World !.\n");
 
-//     return 0;
-// }
+    int tuoi = 27;
+    float chieu_cao = 1.75;
+    char ten[] = "Ngk";
 
+    printf("Ten toi la %s, toi nam nay %d tuoi, toi cao khoang %.2f m.\n", ten, tuoi, chieu_cao);
+
+    return 0;
+}

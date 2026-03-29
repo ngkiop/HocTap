e2e13ae69ec2ba982871972289570e78f45bde08
Ngk <ngocztoong18@gmail.com>
2026-03-29 12:38:06 +0700

    Cap nhat

--- 2_Hoc_tap_c_c++/ngay2.c
+++ 2_Hoc_tap_c_c++/ngay2.c
@@ -1,24 +1,46 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int tuoi;
+//     float diem;
+//     char ky_tu_dau;
+
+//     printf("Nhap tuoi cua toi: ");
+//     scanf("%d", &tuoi);
+
+//     printf("Nhap diem cua toi: ");
+//     scanf("%f", &diem);
+
+//     printf("Nhap ky tu dau trong ten toi: ");
+//     scanf(" %c", &ky_tu_dau);
+
+//     printf("\n--- Thong tin vua nhap ---\n");
+//     printf("Tuoi: %d\n", tuoi);
+//     printf("Diem: %.2f\n", diem);
+//     printf("Ky tu dau: %c\n", ky_tu_dau);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
-    int tuoi;
-    float diem;
-    char ky_tu_dau;
 
-    printf("Nhap tuoi cua toi: ");
-    scanf("%d", &tuoi);
+    float chieu_dai, chieu_rong, chuvi, dientich;
 
-    printf("Nhap diem cua toi: ");
-    scanf("%f", &diem);
+    printf("Nhap tu ban phim chieu dai hinh chu nhat: ");
+    scanf("%f", &chieu_dai);
 
-    printf("Nhap ky tu dau trong ten toi: ");
-    scanf(" %c", &ky_tu_dau);
+    printf("Nhap tu ban phim chieu rong hinh chu nhat: ");
+    scanf("%f", &chieu_rong);
 
-    printf("\n--- Thong tin vua nhap ---\n");
-    printf("Tuoi: %d\n", tuoi);
-    printf("Diem: %.2f\n", diem);
-    printf("Ky tu dau: %c\n", ky_tu_dau);
+    chuvi = (chieu_dai + chieu_rong) * 2;
+    dientich = chieu_dai * chieu_rong;
+
+    printf("Chu vi cua hinh chu nhat la: %.2f\n", chuvi);
+    printf("Dien tich cua hinh chu nhat la: %.2f\n", dientich);
 
     return 0;
 }

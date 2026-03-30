584beeee489ff6fd34ee1caf760bb4ef2ef16936
Ngk <ngocztoong18@gmail.com>
2026-03-30 19:52:11 +0700

    Cap nhat ngay 3

--- 2_Hoc_tap_c_c++/ngay3.c
+++ 2_Hoc_tap_c_c++/ngay3.c
@@ -1,66 +1,21 @@
-// #include <stdio.h>
-
-// int main() {
-
-//     int n;
-//     printf("Nhap so nguyen n: ");
-//     scanf("%d", &n);
-
-//     if (n % 2 == 0) {
-//         printf("%d la so chan.\n", n);
-//     } else {
-//         printf("%d la so le.\n", n);
-//     }
-
-//     return 0;
-
-// }
-
-// #include <stdio.h>
-
-// int main()
-// {
-
-//     float diem;
-//     printf("Nhap diem cua toi: ");
-//     scanf("%f", &diem);
-
-//     if (diem < 0 || diem > 10)
-//     {
-//         printf("Diem khong hop le.\n");
-//     }
-//     else if (diem >= 5)
-//     {
-//         printf("Chuc mung ban da dau.\n");
-//     }
-//     else
-//     {
-//         printf("Rat tiec, ban phai hoc lai.\n");
-//     }
-
-//     return 0;
-// }
-
 #include <stdio.h>
 
 int main()
 {
-    int a, b;
-    printf("Nhap so a va b: ");
-    scanf("%d%d", &a, &b);
 
-    if (a > b)
-    {
-        printf("%d lon hon %d.\n", a, b);
-    }
-    else if (a < b)
-    {
-        printf("%d nho hon %d.\n", a, b);
-    }
-    else
-    {
-        printf("%d bang %d.\n", a, b);
-    }
+    int cd, cr, chuVi, dienTich;
+
+    printf("Nhap tu ban phim chieu dai hinh chu nhat: ");
+    scanf("%d", &cd);
+
+    printf("Nhap tu ban phim chieu rong hinh chu nhat: ");
+    scanf("%d", &cr);
+
+    chuVi = (cd + cr) * 2;
+    dienTich = cd * cr;
+
+    printf("Chu vi hinh chu nhat la: %d.\n", chuVi);
+    printf("Dien tich hinh chu nhat la: %d.\n", dienTich);
 
     return 0;
 }

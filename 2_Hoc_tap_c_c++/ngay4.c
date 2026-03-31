5a4b225c905b094da291673c2933ec1ffc1f2264
Ngk <ngocztoong18@gmail.com>
2026-03-31 20:43:34 +0700

    Cap nhat ngay 4

--- 2_Hoc_tap_c_c++/ngay4.c
+++ 2_Hoc_tap_c_c++/ngay4.c
@@ -1,30 +1,46 @@
 // #include <stdio.h>
+// #include <stdbool.h>
 
 // int main()
 // {
-//     for (int i = 1; i <= 5; i++)
-//     {
-//         printf("Lan thu %d: To dang hoc lap trinh C.\n", i);
-//     }
+
+//     bool la_sinh_vien = true;
+//     bool da_ket_thuc = false;
+
+//     printf("Trang thai sinh vien: %d\n", la_sinh_vien);
+//     printf("Trang thai ket thuc: %d\n", da_ket_thuc);
 
 //     return 0;
 // }
 
+// #include <stdio.h>
+// #include <stdbool.h>
 
-#include<stdio.h>
+// int main()
+// {
+//     int x = 15, y = 20;
 
-int main(){
-    int n, tong = 0;
-    printf("Nhap n tu ban phim: ");
-    scanf("%d", &n);
+//     bool ket_qua_1 = (x < y);
+//     bool ket_qua_2 = (x == y);
+//     bool ket_qua_3 = (x != y);
 
-    for (int i = 1; i <= n; i++)
-    {
-        tong += i;
-    }
-    
-    printf("Tong tu 1 den %d la: %d", n, tong);
+//     printf("15 nho hon 20? Ket qua: %d\n", ket_qua_1);
+//     printf("15 bang 20? Ket qua: %d\n", ket_qua_2);
+//     printf("15 khac 20? Ket qua: %d\n", ket_qua_3);
+
+//     return 0;
+// }
+
+#include <stdio.h>
+//#include <stdbool.h>
+
+int main()
+{
+
+    int m = 5, n = 5;
+
+    printf("%d\n", m >= n);
+    printf("%d\n", m != n);
 
     return 0;
-
 }
